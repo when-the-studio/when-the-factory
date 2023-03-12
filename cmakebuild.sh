e93a7266059e99bd0a73d60a84030265a54511b6
matt <bassetmatt@gmail.com>
2023-03-12 15:19:30 -0400

    Repo init

---
+++ cmakebuild.sh
@@ -0,0 +1,37 @@
+build() {
+	mkdir -p build
+	cd build
+	cmake ..
+	make all
+	compilation_result=$?
+	cd ..
+
+	if [ $compilation_result -ne 0 ]
+	then
+		# Compilation failed, we probably want to stop here in that case.
+		echo "\e[31m\e[1mCompilation failed >.<\e[22m\e[39m"
+		exit 1
+	fi
+}
+
+
+help_function() {
+	echo "aled"
+}
+
+run() {
+	cd bin
+	./wtf
+	cd ..
+}
+
+
+build
+## Parsing Command Line arguments
+while getopts 'rh?' a
+do
+	case $a in
+		r) run;;
+		h) help_function;;
+	esac
+done
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "utils.h"
+
+int main() {
+    printf("Yo!\n");
+    return 0;
+}
